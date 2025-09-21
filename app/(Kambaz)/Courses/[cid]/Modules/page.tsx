da04c50485933bfa249a2bd2cbc6122ac30ec61f
ThomasRoch18 <thomascroch18@gmail.com>
2025-09-21 13:34:56 -0400

    Implemented collapse all, view progress, etc. buttons

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,7 +1,12 @@
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
+      <button>Collpase All</button>
+      <button>View Progress</button>
+      <select>
+        <option value="Publish All">Publish All</option>
+      </select>
+      <button>+ Module</button>
       <ul id="wd-modules">
         <li className="wd-module">
           <div className="wd-title">Week 1</div>

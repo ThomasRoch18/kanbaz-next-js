6afd2eff0c45e50b1cfe7162deaa07105e06a5dc
ThomasRoch18 <thomascroch18@gmail.com>
2025-09-21 13:16:39 -0400

    Added breaks to form button list for home status

--- app/(Kambaz)/Courses/[cid]/Home/tatus.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/tatus.tsx
@@ -2,14 +2,12 @@ export default function CourseStatus() {
   return (
     <div id="wd-course-status">
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      <ul id="wd-course-status">
-        <li><button>Import Existing Content</button></li>
-        <li><button>Import from Commons</button></li>
-        <li><button>Choose Home Page</button></li>
-        <li><button>View Course Stream</button></li>
-        <li><button>New Announcements</button></li>
-        <li><button>New Analytics</button></li>
-        <li><button>View Course Notifications</button></li>
-      </ul>
+      <button>Unpublish</button> <button>Publish</button><br/>
+      <button>Import Existing Content</button><br/>
+      <button>Import from Commons</button><br/>
+      <button>Choose Home Page</button><br/>
+      <button>View Course Stream</button><br/>
+      <button>New Announcements</button><br/>
+      <button>New Analytics</button><br/>
+      <button>View Course Notifications</button>
     </div> );}

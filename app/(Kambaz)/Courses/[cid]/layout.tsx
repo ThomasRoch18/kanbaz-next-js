f1ea7ae78fd400b6966bed8df91355cec54ff5f1
ThomasRoch18 <thomascroch18@gmail.com>
2025-11-05 23:19:07 -0500

    State added to courses and modules

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,21 +1,29 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useState } from "react";
 import { FaAlignJustify } from "react-icons/fa";
 import CourseNavigation from "./Navigation";
-import { courses } from "../../Database";
-import Breadcrumb from "./Breadcrumb";
-export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
- const { cid } = await params;
- const course = courses.find((course) => course._id === cid);
+import { useSelector } from "react-redux";
+import { useParams } from "next/navigation";
+import { RootState } from "../../store";
+export default function CoursesLayout({ children }: { children: ReactNode }) {
+ const { cid }  = useParams();
+ const { courses } = useSelector((state: RootState) => state.coursesReducer);
+ const course = courses.find((course: any) => course._id === cid);
+
+ const [showNav, setShowNav] = useState(true);
+
  return (
    <div id="wd-courses">
     <h2 className="text-danger">
-        <FaAlignJustify className="me-4 fs-4 mb-1" />
-        <Breadcrumb course={course} /> </h2> <hr/>
+        <FaAlignJustify className="me-4 fs-4 mb-1" 
+        role="button"
+          onClick={() => setShowNav(!showNav)}/>
+        {course?.name}</h2> <hr/>
     
     <div className="d-flex">
-      <div className="d-none d-md-block">
-        <CourseNavigation params={{cid}} />
+      <div className={showNav ? "d-block" : "d-none"}>
+        <CourseNavigation params={{cid: Array.isArray(cid) ? cid[0] : cid ?? ""}} />
       
       </div>
       <div className="flex-fill">
--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,13 +1,34 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import * as db from "../Database";
-import { Button, Card, CardBody, CardImg, CardText, CardTitle, Col, Row } from "react-bootstrap";
+import { Button, Card, CardBody, CardImg, CardText, CardTitle, Col, FormControl, Row } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewCourse, deleteCourse, updateCourse, setCourses } from "../Courses/Reducer";
+import { RootState } from "../store";
 export default function Dashboard() {
-  const courses = db.courses;
+  const { courses } = useSelector((state: RootState) => state.coursesReducer);
+  const dispatch = useDispatch();
+  const [course, setCourse] = useState<any>(db.courses);
+  
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>New Course
+          <button className="btn btn-primary float-end"
+                  id="wd-add-new-course-click"
+                  onClick={() => dispatch(addNewCourse(course))} > Add </button>
+          <button className="btn btn-warning float-end me-2"
+                onClick={() => dispatch(updateCourse(course))} id="wd-update-course-click">
+          Update </button>
+
+      </h5><br />
+      <FormControl value={course.name} className="mb-2" 
+                  onChange={(e) => setCourses({ ...course, name: e.target.value }) }/>
+      <FormControl as="textarea" value={course.description} rows={3}
+                  onChange={(e) => setCourses({ ...course, description: e.target.value }) }/>
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
@@ -23,6 +44,22 @@ export default function Dashboard() {
                     <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
                       {course.description} </CardText>
                     <Button variant="primary"> Go </Button>
+                    <Button onClick={(event) => {
+                      event.preventDefault();
+                      dispatch(deleteCourse(course._id));
+                        }} className="btn btn-danger float-end"
+                        id="wd-delete-course-click">
+                      Delete
+                    </Button>
+                    <button id="wd-edit-course-click"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourses(course);
+                      }}
+                      className="btn btn-warning me-2 float-end" >
+                      Edit
+                    </button>
+
                   </CardBody>
                 </Link>
               </Card>
